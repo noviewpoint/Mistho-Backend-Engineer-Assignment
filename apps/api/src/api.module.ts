3b664892a0cdbb3dc29cf6c971153180685f40ce
David <db9949@student.uni-lj.si>
2022-03-19 09:03:44 +0100

    Added RMQ Client to API module

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -4,9 +4,15 @@ import { ApiService } from './api.service';
 import { UtilsModule } from '../../../libs/utils/src';
 import { DatabaseClientModule } from '../../../libs/database-client/src';
 import { SharedConfigurationModule } from '../../../libs/shared-configuration/src';
+import { MessageBrokerClientModule } from '@app/message-broker-client';
 
 @Module({
-  imports: [UtilsModule, DatabaseClientModule, SharedConfigurationModule],
+  imports: [
+    UtilsModule,
+    DatabaseClientModule,
+    SharedConfigurationModule,
+    MessageBrokerClientModule,
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
