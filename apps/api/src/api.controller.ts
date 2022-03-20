a5a35e85d92c799862b71d62cf2712685406baec
David <db9949@student.uni-lj.si>
2022-03-20 11:53:05 +0100

    API endpoinst: download pdf endpoint, start scraping endpoint

--- apps/api/src/api.controller.ts
+++ apps/api/src/api.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Inject,
+  Post,
+} from '@nestjs/common';
 import { ApiService } from './api.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { ApiTags } from '@nestjs/swagger';
+import { createReadStream } from 'fs';
 
-@Controller()
+@ApiTags('api')
+@Controller('api')
 export class ApiController {
-  constructor(private readonly appService: ApiService) {}
+  constructor(
+    private readonly appService: ApiService,
+    @Inject('MESSAGE_BROKER_CLIENT') private client: ClientProxy,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  @HttpCode(202)
+  async scrape(): Promise<string> {
+    await this.client.emit({ cmd: 'scrape' }, 'glassdoor');
+    return 'OK';
+  }
+
+  @Get('downloadPdf')
+  @HttpCode(200)
+  @Header('Content-Type', 'application/pdf')
+  @Header(
+    'Content-Disposition',
+    'attachment; filename=Ravi_Van_server_copy.pdf.pdf',
+  )
+  async downloadPdf() {
+    return createReadStream(`${process.cwd()}/Ravi_Van.pdf`);
   }
 }
